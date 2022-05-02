4ab252379d8356fb26b45306138faacc4b28536d
Azariel Moon <abdiel_18luna@outlook.com>
2022-05-02 15:02:09 -0700

    Agregar Proyecto

---
+++ MenuFicha.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace MarDeCortezDsk
+{
+    partial class MenuFicha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb_data = new System.Windows.Forms.Label();
+            this.lb_time = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtGvw_Ficha_Temp = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btt_enviar = new System.Windows.Forms.Button();
+            this.dtTmpct_datatime = new System.Windows.Forms.DateTimePicker();
+            this.column_Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column_cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bttn_other = new System.Windows.Forms.Button();
+            this.bttn_fish = new System.Windows.Forms.Button();
+            this.btt_Shrim = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGvw_Ficha_Temp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lb_data);
+            this.panel1.Controls.Add(this.lb_time);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(22, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 102);
+            this.panel1.TabIndex = 0;
+            // 
+            // lb_data
+            // 
+            this.lb_data.AutoSize = true;
+            this.lb_data.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_data.Location = new System.Drawing.Point(159, 54);
+            this.lb_data.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_data.Name = "lb_data";
+            this.lb_data.Size = new System.Drawing.Size(141, 36);
+            this.lb_data.TabIndex = 5;
+            this.lb_data.Text = "dd/mm/yy";
+            // 
+            // lb_time
+            // 
+            this.lb_time.AutoSize = true;
+            this.lb_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_time.Location = new System.Drawing.Point(181, 13);
+            this.lb_time.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_time.Name = "lb_time";
+            this.lb_time.Size = new System.Drawing.Size(91, 36);
+            this.lb_time.TabIndex = 4;
+            this.lb_time.Text = "00:00";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(441, 10);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(478, 102);
+            this.panel2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(321, 35);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 36);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "---------";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(183, 35);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 36);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(107, 35);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 36);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "----";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 36);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ficha: ";
+            // 
+            // dtGvw_Ficha_Temp
+            // 
+            this.dtGvw_Ficha_Temp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGvw_Ficha_Temp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.column_Producto,
+            this.column_cantidad});
+            this.dtGvw_Ficha_Temp.Location = new System.Drawing.Point(208, 130);
+            this.dtGvw_Ficha_Temp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtGvw_Ficha_Temp.Name = "dtGvw_Ficha_Temp";
+            this.dtGvw_Ficha_Temp.RowHeadersWidth = 51;
+            this.dtGvw_Ficha_Temp.RowTemplate.Height = 24;
+            this.dtGvw_Ficha_Temp.Size = new System.Drawing.Size(710, 347);
+            this.dtGvw_Ficha_Temp.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(603, 495);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 49);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Cancelar";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // btt_enviar
+            // 
+            this.btt_enviar.BackColor = System.Drawing.Color.Gainsboro;
+            this.btt_enviar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btt_enviar.Location = new System.Drawing.Point(768, 495);
+            this.btt_enviar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btt_enviar.Name = "btt_enviar";
+            this.btt_enviar.Size = new System.Drawing.Size(151, 49);
+            this.btt_enviar.TabIndex = 7;
+            this.btt_enviar.Text = "Enviar";
+            this.btt_enviar.UseVisualStyleBackColor = false;
+            this.btt_enviar.Click += new System.EventHandler(this.btt_enviar_Click);
+            // 
+            // dtTmpct_datatime
+            // 
+            this.dtTmpct_datatime.CalendarMonthBackground = System.Drawing.Color.Transparent;
+            this.dtTmpct_datatime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtTmpct_datatime.Location = new System.Drawing.Point(226, 519);
+            this.dtTmpct_datatime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtTmpct_datatime.Name = "dtTmpct_datatime";
+            this.dtTmpct_datatime.Size = new System.Drawing.Size(272, 26);
+            this.dtTmpct_datatime.TabIndex = 3;
+            this.dtTmpct_datatime.Visible = false;
+            // 
+            // column_Producto
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(20);
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.column_Producto.DefaultCellStyle = dataGridViewCellStyle1;
+            this.column_Producto.HeaderText = "Producto";
+            this.column_Producto.MinimumWidth = 6;
+            this.column_Producto.Name = "column_Producto";
+            this.column_Producto.ReadOnly = true;
+            this.column_Producto.Width = 512;
+            // 
+            // column_cantidad
+            // 
+            this.column_cantidad.HeaderText = "Cantidad";
+            this.column_cantidad.Name = "column_cantidad";
+            this.column_cantidad.Width = 166;
+            // 
+            // bttn_other
+            // 
+            this.bttn_other.BackColor = System.Drawing.Color.Gainsboro;
+            this.bttn_other.BackgroundImage = global::MarDeCortezDsk.Properties.Resources.aquarium_octopus;
+            this.bttn_other.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bttn_other.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn_other.Location = new System.Drawing.Point(22, 415);
+            this.bttn_other.Margin = new System.Windows.Forms.Padding(2);
+            this.bttn_other.Name = "bttn_other";
+            this.bttn_other.Size = new System.Drawing.Size(152, 128);
+            this.bttn_other.TabIndex = 4;
+            this.bttn_other.UseVisualStyleBackColor = false;
+            // 
+            // bttn_fish
+            // 
+            this.bttn_fish.BackColor = System.Drawing.Color.Gainsboro;
+            this.bttn_fish.BackgroundImage = global::MarDeCortezDsk.Properties.Resources.fish;
+            this.bttn_fish.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bttn_fish.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttn_fish.Location = new System.Drawing.Point(22, 266);
+            this.bttn_fish.Margin = new System.Windows.Forms.Padding(2);
+            this.bttn_fish.Name = "bttn_fish";
+            this.bttn_fish.Size = new System.Drawing.Size(152, 128);
+            this.bttn_fish.TabIndex = 3;
+            this.bttn_fish.UseVisualStyleBackColor = false;
+            this.bttn_fish.Click += new System.EventHandler(this.bttn_fish_Click);
+            // 
+            // btt_Shrim
+            // 
+            this.btt_Shrim.BackColor = System.Drawing.Color.Gainsboro;
+            this.btt_Shrim.BackgroundImage = global::MarDeCortezDsk.Properties.Resources.shrimp;
+            this.btt_Shrim.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btt_Shrim.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btt_Shrim.Location = new System.Drawing.Point(22, 124);
+            this.btt_Shrim.Margin = new System.Windows.Forms.Padding(2);
+            this.btt_Shrim.Name = "btt_Shrim";
+            this.btt_Shrim.Size = new System.Drawing.Size(152, 128);
+            this.btt_Shrim.TabIndex = 2;
+            this.btt_Shrim.UseVisualStyleBackColor = false;
+            this.btt_Shrim.Click += new System.EventHandler(this.btt_Shrim_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MarDeCortezDsk.Properties.Resources.LOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MenuFicha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(941, 553);
+            this.Controls.Add(this.btt_enviar);
+            this.Controls.Add(this.dtTmpct_datatime);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dtGvw_Ficha_Temp);
+            this.Controls.Add(this.bttn_other);
+            this.Controls.Add(this.bttn_fish);
+            this.Controls.Add(this.btt_Shrim);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MenuFicha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MenuFicha";
+            this.Load += new System.EventHandler(this.MenuFicha_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGvw_Ficha_Temp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btt_Shrim;
+        private System.Windows.Forms.Button bttn_fish;
+        private System.Windows.Forms.Button bttn_other;
+        private System.Windows.Forms.DataGridView dtGvw_Ficha_Temp;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btt_enviar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dtTmpct_datatime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_data;
+        private System.Windows.Forms.Label lb_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column_Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column_cantidad;
+    }
+}
---
+++ MenuFicha.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarDeCortezDsk
+{
+    public partial class MenuFicha : Form
+    {
+        public MenuFicha()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuFicha_Load(object sender, EventArgs e)
+        {
+            lb_time.Text = (dtTmpct_datatime.Value).ToString();
+        }
+
+        private void btt_enviar_Click(object sender, EventArgs e)
+        {
+            Form bettascreen = new MenuEleccion();
+            this.Hide();
+            bettascreen.Show();
+        }
+
+        private void btt_Shrim_Click(object sender, EventArgs e)
+        {
+            Form bettafichashirim = new camaronlist();
+            bettafichashirim.Show();
+        }
+
+        private void bttn_fish_Click(object sender, EventArgs e)
+        {
+            Form bettafichafish = new peceslist();
+            bettafichafish.Show();
+        }
+    }
+}
